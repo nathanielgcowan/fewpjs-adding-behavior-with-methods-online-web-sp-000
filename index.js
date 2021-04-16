bec9f646ee551d35bc6c695eb497cbc14555af3e
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-16 04:35:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,4 +22,11 @@ class Bird {
     this.name = name;
     this.sex = sex;
   }
+  speak() {
+    if (this.sex === "male" ) {
+      return `It's me! ${this.name} the parrot!`;
+    } else {
+      return `Squawk!`
+    }
+  }
 }
