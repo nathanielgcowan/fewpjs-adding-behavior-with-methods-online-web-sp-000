e412d384033a3957622e34d98f33d43d155fad79
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-16 04:25:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Your code here
-class Cat {}
-class Dog {}
-class Bird {}
+class Cat {
+  constructor(name, sex) {
+  this.name = name;
+  this.sex = sex;
+}
+class Dog {
+  constructor(name, sex) {
+  this.name = name;
+  this.sex = sex;
+}
+class Bird {
+  constructor(name, sex) {
+  this.name = name;
+  this.sex = sex;
+}
